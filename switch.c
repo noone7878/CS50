c4b11eea86d84b16823b289dc37ca1d6b84f004b
noone7878 <ny4621603@gmail.com>
2024-11-23 06:33:44 +0530

    switch

---
+++ switch.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    int day = 3;
+
+    switch (day) {
+        case 1:
+            printf("its Monday");
+            break;
+        case 2:
+            printf("its Tuesday");
+            break;
+        case 3:
+            printf("its Wednusday");
+            break;
+            case 4:
+              printf("Thursday");
+              break;
+            case 5:
+              printf("Friday");
+              break;
+            case 6:
+              printf("Saturday");
+              break;
+            case 7:
+              printf("Sunday");
+              break;
+    }
+    return 0;
+}
